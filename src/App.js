12e1ea734ad75e6b9df248b797b1e48d05622983
Eddie Olivas <eddie@chrysaliswebdevelopment.com>
2021-06-24 10:52:57 -0700

    Initial set up. React & React DOM installed.

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <div class="exercise-header">
+        <h2>Exercise 1 - Testimonial Block</h2>
+      </div>
+
+      <div>
+        <div>
+          <div>
+            <p>Gingerbread tart cupcake cake muffin cookie liquorice tiramisu. Toffee cupcake cake cake croissant icing carrot cake cookie. Dessert chocolate bar apple pie sesame snaps liquorice carrot cake cookie danish.</p>
+            <span>Indiana Jones, Archaeologist</span>
+          </div>
+          <a>Tell Me More</a>
+        </div>
+      </div>
+
+      <div class="exercise-header">
+        <h2>Exercise 2 - Filterable Content</h2>
+      </div>
+
+      <div>[Exercise 2 Here]</div>
+    </>
+  );
+}
+
+export default App;
