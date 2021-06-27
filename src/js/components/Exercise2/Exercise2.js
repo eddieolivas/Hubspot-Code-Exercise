2145e8e1ad5cd12326429c5af071bac7a714b4db
Eddie Olivas <eddie@chrysaliswebdevelopment.com>
2021-06-27 13:02:00 -0700

    Filter functionality works and mobile responsive styles in place.

--- src/js/components/Exercise2/Exercise2.js
+++ src/js/components/Exercise2/Exercise2.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import MultiSelect from "react-multi-select-component";
 import MediaItems from "./MediaItems";
 import styles from './Exercise2.module.scss';
+import search from 'url:../../../assets/images/search.png';
 
 const Exercise2 = () => {
   const genres = [
@@ -25,81 +26,55 @@ const Exercise2 = () => {
     { label: "1974", value: "1974" },
   ];
 
+  // Custom value rendered for multi-select field.
+  const valueRenderer = (value) => value;
+
   const [genre, setGenre] = useState([]);
   const [year, setYear] = useState([]);
   const [type, setType] = useState('');
-  const [searchTitle, setSearchTitle] = useState(null);
-  const [mediaItems, setMediaItems] = useState([]);
-  const [filteredItems, setFilteredItems] = useState(mediaItems);
-
-  const genreValueRenderer = (selected, _options) => {
-    return "Genre";
-  };
-
-  const yearValueRenderer = (selected, _options) => {
-    return "Year";
-  };
+  const [searchTitle, setSearchTitle] = useState('');
+  const [originalItems, setOriginalItems] = useState([]);
+  const [filteredItems, setFilteredItems] = useState(originalItems);
+  const [clearFilters, setClearFilters] = useState(false);
 
   useEffect(() => {
     const apiUrl = 'https://raw.githubusercontent.com/HubSpotWebTeam/CodeExercise/main/src/js/data/data.json';
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data.media);
-        setMediaItems(data.media);
+        setOriginalItems(data.media);
         setFilteredItems(data.media);
       });
   }, []);
 
   useEffect(() => {
-    if(!genre.length) {
-      setFilteredItems(mediaItems);
+    if (clearFilters) {
+      setFilteredItems(originalItems);
+      setClearFilters(false);
       return;
     }
-    console.log('Genre was changed to: ' + JSON.stringify(genre));
-    const filterGenres = genre.map(genre => genre.value);
-    const updatedItems = mediaItems.filter(item => {
-      return filterGenres.some(genre => item.genre.includes(genre));
-    });
-    setFilteredItems(updatedItems);
-  }, [genre]);
+    const filters = {
+      genre: genre.map(genre => genre.value),
+      year: year.map(genre => genre.value),
+      type: type,
+      title: searchTitle
+    };
 
-  useEffect(() => {
-    if(!year) {
-      setFilteredItems(mediaItems);
-      return;
-    }
-    console.log('Year was changed to: ' + JSON.stringify(year));
-    const filterYears = year.map(year => year.value);
-    const updatedItems = mediaItems.filter(item => {
-      return filterYears.some(year => item.year === year);
+    var filterKeys = Object.keys(filters);
+    const updatedItems = originalItems.filter((item) => {
+      return filterKeys.every((key) => {
+        if (!filters[key].length) {
+          return true; 
+        } else if (!Array.isArray(filters[key])) {
+          return item[key].toLowerCase().includes(filters[key].toLowerCase());
+        }
+        return filters[key].some(filterGenre => item[key].includes(filterGenre));
+      });
     });
-    setFilteredItems(updatedItems);
-  }, [year]);
 
-  useEffect(() => {
-    if(!type) {
-      setFilteredItems(mediaItems);
-      return;
-    }
-    console.log('Type was changed to: ' + JSON.stringify(type));
-    const updatedItems = mediaItems.filter(item => {
-      return item.type === type;
-    });
     setFilteredItems(updatedItems);
-  }, [type]);
 
-  useEffect(() => {
-    if(!searchTitle) {
-      setFilteredItems(mediaItems);
-      return;
-    }
-    console.log('Search was changed to: ' + JSON.stringify(searchTitle));
-    const updatedItems = mediaItems.filter(item => {
-      return item.title.toLowerCase().includes(searchTitle.toLowerCase());
-    });
-    setFilteredItems(updatedItems);
-  }, [searchTitle]);
+  }, [genre, year, type, searchTitle]);
 
   const filterTypeHandler = (event) => {
     setType(event.target.value);
@@ -110,57 +85,76 @@ const Exercise2 = () => {
   };
 
   const clearFilterHandler = (event) => {
-    console.log('clear filter handler ran')
     event.preventDefault();
-    setGenre();
-    setYear();
-    setType();
+    setClearFilters(true);
+    setGenre([]);
+    setYear([]);
+    setType('');
     setSearchTitle('');
-    setFilteredItems(mediaItems);
+    setFilteredItems(originalItems);
   };
 
   return (
     <div className={styles.exercise2}>
       <header className={styles.header}>
         <form>
-          <MultiSelect
-            className={styles.multiSelect}
-            disableSearch={true}
-            hasSelectAll={false}
-            options={genres}
-            value={genre}
-            onChange={setGenre}
-            labelledBy="Genre"
-            label="Genre"
-            valueRenderer={genreValueRenderer}
-          />
+          <div className={styles.leftSide}>
+            <MultiSelect
+              className={styles.multiSelect}
+              disableSearch={true}
+              hasSelectAll={false}
+              options={genres}
+              value={genre}
+              onChange={setGenre}
+              labelledBy="Genre"
+              label="Genre"
+              valueRenderer={() => valueRenderer('Genre')}
+            />
 
-          <MultiSelect
-            className={styles.multiSelect}
-            disableSearch={true}
-            hasSelectAll={false}
-            options={years}
-            value={year}
-            onChange={setYear}
-            labelledBy="Year"
-            valueRenderer={yearValueRenderer}
-          />
-          
-          <div className={styles.radioButtons} onChange={filterTypeHandler}>
-            <input type="radio" id="books" name="mediaType" value="book" />
-            <label htmlFor="books">Books</label>
-            <input type="radio" id="movies" name="mediaType" value="movie" />
-            <label htmlFor="movies">Movies</label>
+            <MultiSelect
+              className={styles.multiSelect}
+              disableSearch={true}
+              hasSelectAll={false}
+              options={years}
+              value={year}
+              onChange={setYear}
+              labelledBy="Year"
+              valueRenderer={() => valueRenderer('Year')}
+            />
+            
+            <div className={styles.radioButtons}>
+              <input 
+                type="radio" 
+                id="books" 
+                name="mediaType" 
+                checked={type === "book"} 
+                onChange={filterTypeHandler} 
+                value="book" 
+              />
+              <label htmlFor="books">Books</label>
+              <input 
+                type="radio" 
+                id="movies" 
+                name="mediaType" 
+                checked={type === "movie"} 
+                onChange={filterTypeHandler} 
+                value="movie" 
+              />
+              <label htmlFor="movies">Movies</label>
+            </div>
           </div>
 
-          <div className={styles.search}>
-            <input 
-              value={searchTitle}
-              onChange={searchTitleChangeHandler}
-              className={styles.searchInput} type="text" />
-          </div>
+          <div className={styles.rightSide}>
+            <div className={styles.search}>
+              <input 
+                value={searchTitle}
+                onChange={searchTitleChangeHandler}
+                className={styles.searchInput} type="text" />
+              <button className={styles.searchButton} type="submit" onClick={(e) => e.preventDefault()}><img src={search} /></button>
+            </div>
 
-          <a className={styles.clearFilters} href="" onClick={clearFilterHandler}>Clear Filters</a>
+            <a className={styles.clearFilters} href="" onClick={clearFilterHandler}>Clear Filters</a>
+          </div>
         </form>
       </header>
       <MediaItems items={filteredItems} />
--- src/js/components/Exercise2/MediaItems.js
+++ src/js/components/Exercise2/MediaItems.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './MediaItems.module.scss';
 
 const MediaItems = (props) => {
+  const [urlError, setUrlError] = useState(false);
+
   return (
     <div className={styles.mediaItems}>
       {props.items.map((item) => (
@@ -10,7 +12,10 @@ const MediaItems = (props) => {
           className={styles.mediaItem} 
           key={item.title}
         >
-          <img src={item.poster} />
+          <img 
+            src={item.poster} 
+            onError={(e)=>{e.target.onError = null; e.target.src = 'https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png'}}
+          />
           <p className={styles.itemTitle}>{item.title} ({item.year})</p>
           <p>Genres: {item.genre.length && item.genre.map((genre) => (<span key={genre} className={styles.genre}>{genre} </span>))}</p>
         </div>
